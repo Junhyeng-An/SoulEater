aa295177d86da8ec51bacb4a610ecb5fbd70940c
Park <deapa24@gmail.com>
2023-09-23 12:57:49 +0900

    switch_clear_Park

--- Assets/Script/Switch_v2.cs
+++ Assets/Script/Switch_v2.cs
@@ -6,30 +6,34 @@ public class Switch_v2: MonoBehaviour
 {
     private bool isPlayer; //플레이어의 여부
     public float speed = 5; //이동속도
+    public float jumpforce = 20.0f;
 
     public Color playerColor = Color.blue; // 플레이어 색상
     public Color nonPlayerColor = Color.red; // 플레이어가 아닌 경우의 색상
     private SpriteRenderer spriteRenderer;
+    Rigidbody2D rb;
 
-    bool GetSword;
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
     private void Update()
     {
-        if (GetSword == true)
         if (gameObject.tag == "Player")
         {
-            isPlayer = true;
-            GetSword = true;
             float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-            Vector2 movement = new Vector2(horizontalInput, verticalInput);
-            GetComponent<Rigidbody2D>().velocity = movement * speed;
+            Vector2 movement = new Vector2(horizontalInput, 0);
+            rb.velocity = new Vector2(movement.x * speed, rb.velocity.y); // x 속도는 움직임에 따라 변경하고, y 속도는 그대로 유지
+
             spriteRenderer.color = playerColor;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+            }
         }
         else
         {
@@ -37,16 +41,21 @@ public class Switch_v2: MonoBehaviour
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             spriteRenderer.color = nonPlayerColor;
         }
+
+        //if (Input.GetKeyDown("q") && gameObject.tag == "Player")
+        //{
+        //    GetSword = false;
+        //}
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-            //if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Player"))
-            //{
-            //    // 충돌 대상의 태그도 변경합니다.
-            //    collision.gameObject.tag = isPlayer ? "Player" : "Enemy";
-            //    gameObject.tag = isPlayer ? "Enemy" : "Player";
-            //    Debug.Log("변경됨");
-            //}
+        if (collision.gameObject.CompareTag("Sword") || collision.gameObject.CompareTag("Player"))
+        {
+            // 충돌 대상의 태그도 변경합니다.
+            collision.gameObject.tag = isPlayer ? "Player" : "Enemy";
+            gameObject.tag = isPlayer ? "Enemy" : "Player";
+            Debug.Log("변경됨");
+        }
     }
 }
--- Assets/Script/Sword_V2.cs
+++ Assets/Script/Sword_V2.cs
@@ -1,29 +1,40 @@
+using System.Collections;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
 public class Sword_V2 : MonoBehaviour
 {
-    public float force = 5.0f;
+    public float force = 25.0f;
     float angle;
-
+    Rigidbody2D rb;
+    GameObject P; //플레이어
     [HideInInspector]
     public float angle2;
     bool isThrowing = false;
+    float throwStartTime = 0f;
     Vector2 mouse;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
+        P = GameObject.FindWithTag("Player"); //플레이어를 찾음
         if (!isThrowing)
         {
             GameObject target = GameObject.FindGameObjectWithTag("Player");
-            //GameObject player = GameObject.FindGameObjectWithTag("Player");
-            //transform.position = player.transform.position;
+
+            //플레이어 주위로 칼을 돌리는 코드 ////////////////////////
 
             mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             angle = Mathf.Atan2(mouse.y - target.transform.position.y, mouse.x - target.transform.position.x) * Mathf.Rad2Deg;
-            this.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+            this.transform.rotation = Quaternion.AngleAxis(angle , Vector3.forward);
             Vector2 v2 = new Vector2(mouse.y - target.transform.position.y, mouse.x - target.transform.position.x);
             angle2 = Mathf.Atan2(v2.x, v2.y);
             transform.position = new Vector2(target.transform.position.x + Mathf.Cos(angle2), target.transform.position.y + Mathf.Sin(angle2));
+
+            /////////////////////////////////////////////////////////
+            
 
             if (Input.GetMouseButton(0))
             {
@@ -45,42 +56,59 @@ public class Sword_V2 : MonoBehaviour
                 GetComponent<SpriteRenderer>().color = Color.white;
                 // 마우스 오른쪽 버튼을 뗄 때의 처리
             }
+           
         }
-        if (Input.GetKeyDown("q"))
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); //마우스 위치를 가져옴
+        mousePosition.z = 0f; // Z 좌표를 0으로 설정 (2D 환경)
+
+        // 플레이어의 현재 위치
+        Vector3 playerPosition = transform.position;
+
+        if (Input.GetKeyDown("q") && isThrowing ==false)
         {
             isThrowing = true;
-            Throwing();
+            if (mousePosition.x > playerPosition.x)
+            {
+                // 마우스가 오른쪽에 있을 때 칼을 오른쪽으로 날린다.
+                Throwing(Vector2.right);
+            }
+            else
+            {
+                // 마우스가 왼쪽에 있을 때 칼을 왼쪽으로 날린다.
+                Throwing(Vector2.left);
+            }
+        }
+        if (isThrowing == true) 
+        {
+            throwStartTime += Time.deltaTime; // 시간을 추가합니다.
+            Debug.Log(throwStartTime);
+            if (throwStartTime >= 2.0f) //던지고 3초뒤에 다시 플레이어에게 돌아옴
+            {
+                Debug.Log("돌아옴");
+                isThrowing = false;
+                throwStartTime = 0;
+            }
         }
     }
 
-    private void Throwing()
+    private void Throwing(Vector2 direction)
     {
-        // 마우스 포인터의 위치를 월드 좌표로 가져옵니다.
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        // 현재 오브젝트의 위치를 가져옵니다.
-        Vector3 currentPosition = transform.position;
-
-        // 마우스 포인터 방향 벡터를 계산합니다.
-        Vector3 direction = mousePosition - currentPosition;
-
-        // 힘의 크기를 조절합니다.
-        float forceMagnitude = 50.0f; // 힘의 크기를 원하는 값으로 조절하세요.
-
-        // 방향 벡터를 정규화하고 힘을 가할 Rigidbody2D 컴포넌트를 가져옵니다.
-        direction.Normalize();
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-
-        // 힘을 가합니다. 방향 벡터의 크기에 forceMagnitude를 곱하여 힘을 조절합니다.
-        rb.AddForce(direction * forceMagnitude, ForceMode2D.Impulse);
-
+        rb.velocity = direction.normalized * 10f;
+        //rb.AddForce(direction * force, ForceMode2D.Impulse);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Enemy") 
+        if (collision.gameObject.tag == "Enemy")
         {
-         
+            isThrowing = false;
+            P.tag = collision.gameObject.tag;
+            P.layer = 0;
+            collision.gameObject.tag = "Player";
+            collision.gameObject.layer = 6;
+            Debug.Log("변경됨");
+            throwStartTime = 0;
         }
     }
 

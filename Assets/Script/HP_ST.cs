b0db9c11579ccd4d531722568ae1c655140bd9e8
Park <deapa24@gmail.com>
2023-09-26 22:35:52 +0900

    hp_V2

---
+++ Assets/Script/HP_ST.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP_ST : MonoBehaviour
+{
+    public float CurHP;
+    public float MaxHP;
+    //public GameObject HP_Bar;
+    //public Slider HP_Slider;
+    //public TextMeshProUGUI hp_text;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        //if(gameObject.tag == "Enemy")
+        //{
+        //   HP_Bar.transform.position = transform.root.position + new Vector3(0,2,0);
+        //   HP_Bar.SetActive(true);
+        //   HP_Slider.value = CurHP / MaxHP;
+        //    hp_text.text = CurHP.ToString() + " / " + MaxHP.ToString(); // 현재 체력을 표시합니다.
+        //}
+        //else
+        //{
+        //    HP_Bar.SetActive(false);
+        //}
+    }
+}
---
+++ Assets/Script/StatController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
+
+public class StatController : MonoBehaviour
+{
+    //GameManger에서 관리중
+    [SerializeField]
+    Slider Player_HP;
+    [SerializeField]
+    TextMeshProUGUI pText_hp;
+    [SerializeField]
+    Slider Player_ST;
+    [SerializeField]
+    TextMeshProUGUI pText_ST;
+
+    float Player_MaxHP;
+    float Player_CurHP;
+
+    float Player_MaxST = 10;
+    float Player_CurST = 10;
+    GameObject player;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            //Player 오브젝트에서 Player_MaxHP와 Player_CurHP 값을 가져옵니다.
+            Player_MaxHP = player.GetComponent<HP_ST>().MaxHP;
+            Player_CurHP = player.GetComponent<HP_ST>().CurHP;
+
+            Player_HP.value = Player_CurHP / Player_MaxHP;
+            Player_ST.value = Player_CurST / Player_MaxST;
+            if (Input.GetKeyDown("p")) //hp 데미지
+            {
+                if (Player_CurHP > 0)
+                {
+                    player.GetComponent<HP_ST>().CurHP -= 10;
+                }
+            }
+            if (Input.GetMouseButtonDown(0)) //st 소모
+            {
+                if (Player_CurST > 0)
+                {
+                    Player_CurST -= 3;
+                }
+            }
+            if (Player_CurHP <= 0 || Player_CurST <= 0) //hp, 스테미나 음수 방지
+            {
+                Player_CurST = 0;
+                Player_CurHP = 0;
+            }
+
+            if (Player_CurST < Player_MaxST)
+            {
+                float stRecoveryRate = 0.1f; // 0.1초당 회복량
+                Player_CurST += stRecoveryRate * Time.deltaTime;
+
+                // 현재 ST가 최대 ST를 넘지 않도록 제한합니다.
+                Player_CurST = Mathf.Min(Player_CurST, Player_MaxST);
+            }
+            pText_hp.text = Player_CurHP.ToString() + " / " + Player_MaxHP.ToString(); // 현재 체력을 표시합니다.
+            pText_ST.text = Player_CurST.ToString() + " / " + Player_MaxST.ToString(); // 현재 스테미나를 표시합니다.
+            Handle();
+
+        }
+        
+    }
+
+    void Handle() //hp , st 가 닳는 애니메이션
+    {
+        Player_HP.value = Mathf.Lerp(Player_HP.value, (float)Player_CurHP / (float)Player_MaxHP, Time.deltaTime * 10);
+        Player_ST.value = Mathf.Lerp(Player_ST.value, (float)Player_CurST / (float)Player_MaxST, Time.deltaTime * 10);
+    }
+
+
+}

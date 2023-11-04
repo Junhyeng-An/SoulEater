7f3a5c8daf883e21d9fe883ab32418aabebe1ef7
Park <deapa24@gmail.com>
2023-11-04 13:51:41 +0900

    Enemy_ai_ver1.0

--- Assets/Script/EnemyControlloer.cs
+++ Assets/Script/EnemyControlloer.cs
@@ -4,6 +4,10 @@ using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEditor.Searcher;
+using UnityEditor.SearchService;
+using Unity.VisualScripting;
+
 public class EnemyController : MonoBehaviour
 {
     public GameObject Player;
@@ -14,10 +18,17 @@ public class EnemyController : MonoBehaviour
     public float MaxHP;
     public float CurWP;
     public float MaxWP;
+    public int nextMove;//행동지표를 결정할 변수
+    public float detect_meter = 4.0f;
+    public float attack_meter = 1.2f;
+    public bool issearch = false;
     float height = 0.8f;
+
     [SerializeField]
     Slider Enemy_HP;
 
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
     StatController stat;
 
     public bool isDamage = false;
@@ -67,8 +78,11 @@ public class EnemyController : MonoBehaviour
         rigidPlayer = Player.GetComponent<Rigidbody2D>();
         sword = GameObject.Find("Sword").GetComponent<Sword>();
         Soul_Drop = GetComponent<global::Soul_Drop>();
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Invoke("Think", 1);
     }
-    // Update is called once per frame
     void Update()
     {
         if (gameObject.layer == 12)
@@ -96,6 +110,17 @@ public class EnemyController : MonoBehaviour
         if(sword.swingForce <= 1.5f)
         {
             isDamage = false;
+        }
+        if(gameObject.tag == "Enemy" || gameObject.tag == "Disarmed")
+        {
+            if(issearch == false)
+            {
+                Idle();
+            }
+            else if(issearch == true) 
+            {
+                Enemy_detect();
+            }
         }
     }
     private void OnTriggerEnter2D(Collider2D col)
@@ -157,6 +182,95 @@ public class EnemyController : MonoBehaviour
             Soul_Drop.DropItem();
             this.gameObject.active = false;
             
+        }
+    }
+
+    void Idle() //Enemy ai Idle
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        Vector2 Mypos = transform.position;
+
+        GameObject con = GameObject.FindGameObjectWithTag("Player");
+
+        Vector2 conPos = con.transform.position;
+
+        float distance = Vector2.Distance(Mypos, conPos);
+
+        //Enemy Move 
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 1.2f, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0)); //fall_area check Ray
+
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Floor"));
+
+
+        if (rayHit.collider == null)
+        {
+            Turn();
+        }
+        if(distance <= detect_meter && distance >= attack_meter)
+        {
+            issearch = true;    
+        }
+
+
+    }
+
+    void Think()
+    {
+
+        //set next active
+        nextMove = Random.Range(-1, 2); //-1 = left, 0 = stop ,1 = right
+
+        //change direct
+        if (nextMove != 0)
+        {
+            spriteRenderer.flipX = (nextMove == 1);
+        }
+        if (nextMove == 0)
+        {
+            Invoke("Think", 1.0f);
+        }
+        else
+        {
+
+            float nextThinkTime = Random.Range(2f, 5f);//thinktime cooltime
+
+            Invoke("Think", nextThinkTime);
+        }
+    }
+
+    void Turn() // if Enemyfront == fall_area
+    {
+        nextMove = nextMove * (-1);
+        spriteRenderer.flipX = (nextMove == 1);
+        CancelInvoke();
+        Invoke("Think", 2);
+    }
+    void Enemy_detect()
+    {
+        Vector2 Mypos = transform.position;
+
+        GameObject con = GameObject.FindGameObjectWithTag("Player");
+
+        Vector2 conPos = con.transform.position;
+
+        float distance = Vector2.Distance(Mypos, conPos);
+
+
+        if (distance <= detect_meter && distance >= attack_meter)
+        {
+            Debug.Log("인식됨");
+            transform.position = Vector2.Lerp(transform.position, conPos, 0.4f * Time.deltaTime);
+        }
+        else if (distance <= attack_meter)
+        {
+            //Attack();
+            Debug.Log("플레이어를 향해 공격중");
+        }
+        else
+        {
+            issearch = false;
         }
     }
 }

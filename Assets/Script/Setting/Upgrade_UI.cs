88542cba1b685e37aac8ad9ab45560ad27a1eb48
enemyday <114123065+enemyday@users.noreply.github.com>
2024-01-02 17:36:47 +0900

    Weapon_Button

---
+++ Assets/Script/Setting/Upgrade_UI.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using PixelCrushers;
+using UnityEngine;
+using TMPro;
+public class Upgrade_UI : MonoBehaviour
+{
+    public TextMeshProUGUI Weapon_Attack_Level;
+    public TextMeshProUGUI Weapon_Parrying_Level;
+    public TextMeshProUGUI Weapon_Reach_Level;
+
+
+    private void Update()
+    {
+        Weapon_Attack_Level.text = DataManager.Instance._SwordData.player_attack_level.ToString() + "->"
+            + (DataManager.Instance._SwordData.player_attack_level + 1).ToString();
+        Weapon_Parrying_Level.text = DataManager.Instance._SwordData.player_parrying_level.ToString() + "->"
+            + (DataManager.Instance._SwordData.player_parrying_level + 1).ToString();
+        Weapon_Reach_Level.text = DataManager.Instance._SwordData.player_sword_level.ToString() + "->"
+            + (DataManager.Instance._SwordData.player_sword_level + 1).ToString();
+
+    }
+}

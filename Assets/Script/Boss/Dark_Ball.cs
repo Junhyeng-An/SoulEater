00691273b2e0abfe873b33e0182a5a19d203bcc6
Park_GT <deapa24@gmail.com>
2024-03-06 23:14:42 +0900

    Boss_pattern_Finish

---
+++ Assets/Script/Boss/Dark_Ball.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Dark_Ball : MonoBehaviour
+{
+    float Dark_Ball_Damage = 10;
+    // TODO: knight Boss Dark Damage (Damage to be adjusted later)
+    GameObject player;
+    EnemyController enemyController;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Col_Dark_Ball(collision);
+    }
+    void Col_Dark_Ball(Collider2D col)
+    {
+        player = GameObject.FindGameObjectWithTag("Controlled");
+        enemyController = player.GetComponent<EnemyController>();
+        if (col.tag == "hit_area")
+        {
+            SettingManager.Instance.Damage_Calculate(col, Dark_Ball_Damage, enemyController);
+        }
+    }
+}

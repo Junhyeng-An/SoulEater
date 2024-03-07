e99cc5083d20d48f3b1d7cd87d04f3e7e38880f0
Park_GT <deapa24@gmail.com>
2024-03-07 15:08:24 +0900

    BG_MAP_1-2

--- Assets/Script/Boss/Layzer.cs
+++ Assets/Script/Boss/Layzer.cs
@@ -1,43 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
 
-public class Layzer : MonoBehaviour
-{
-    float Bullet_Damage = 10;
-    // TODO: Bat Boss Laser Damage (Damage to be adjusted later)
-    float Boss_Bullet_Damage = 100;
-    // TODO: Night Boss Laser Damage (Damage to be adjusted later)
-    GameObject player;
-    EnemyController enemyController;
-    // Start is called before the first frame update
-    void Start()
+    public class Layzer : MonoBehaviour
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Col_Layzer(collision);
-    }
-    void Col_Layzer(Collider2D col)
-    {
-        player = GameObject.FindGameObjectWithTag("Controlled");
-        enemyController = player.GetComponent<EnemyController>();
-        if (col.gameObject.tag == "Player")
+        float Bullet_Damage = 10;
+        // TODO: Bat Boss Laser Damage (Damage to be adjusted later)
+        float Boss_Bullet_Damage = 100;
+        // TODO: Night Boss Laser Damage (Damage to be adjusted later)
+        GameObject player;
+        EnemyController enemyController;
+        // Start is called before the first frame update
+        void Start()
         {
-            SettingManager.Instance.Damage_Calculate(col, Bullet_Damage, enemyController);
-        }
-        if (col.gameObject.tag == "Player" && SceneManager.GetActiveScene().name == "Boss3")
-        {
-            SettingManager.Instance.Damage_Calculate(col, Boss_Bullet_Damage, enemyController);
+
         }
 
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Col_Layzer(collision);
+        }
+        void Col_Layzer(Collider2D col)
+        {
+            player = GameObject.FindGameObjectWithTag("Controlled");
+            if (player != null) 
+            {
+                enemyController = player.GetComponent<EnemyController>();
+                if (enemyController != null)
+                {
+                    if (col.gameObject.tag == "Player")
+                    {
+                        SettingManager.Instance.Damage_Calculate(col, Bullet_Damage, enemyController);
+                    }
+                    if (col.gameObject.tag == "Player" && SceneManager.GetActiveScene().name == "Boss3")
+                    {
+                        SettingManager.Instance.Damage_Calculate(col, Boss_Bullet_Damage, enemyController);
+                    }
+                }
+             }
+        }
     }
-}

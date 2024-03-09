311333063844077ed140ab96b162e3f2fc949316
Park_GT <deapa24@gmail.com>
2024-03-09 14:46:51 +0900

    bg`

--- Assets/Script/Setting/Singleton/Main_Camera_Singleton.cs
+++ Assets/Script/Setting/Singleton/Main_Camera_Singleton.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Com.LuisPedroFonseca.ProCamera2D;
+using UnityEngine.SceneManagement;
 
 public class Main_Camera_Singleton : MonoBehaviour
 {
